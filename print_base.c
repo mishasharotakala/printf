919a0f927f7e2a78cce94038c1ed0b1269886b42
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-05-18 22:00:26 +0200

    team task

--- print_base.c
+++ print_base.c
@@ -34,11 +34,6 @@ int print_hex_big(va_list l, flags_t *f)
 
 /**
  * print_binary - prints a number in base 2
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
 int print_binary(va_list l, flags_t *f)
@@ -52,11 +47,6 @@ int print_binary(va_list l, flags_t *f)
 
 /**
  * print_octal - prints a number in base 8
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
 int print_octal(va_list l, flags_t *f)
