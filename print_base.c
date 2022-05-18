64aa8722c724fe39a3f868b4d68150bf4e170c2b
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-05-18 21:59:07 +0200

    team task

--- print_base.c
+++ print_base.c
@@ -1,13 +1,7 @@
 #include "main.h"
 
 /**
- * print_hex - prints a number in hexadecimal base,
- * in lowercase
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
+ * print_hex - prints a number in hexadecimal base
  * Return: the number of char printed
  */
 int print_hex(va_list l, flags_t *f)
@@ -23,13 +17,7 @@ int print_hex(va_list l, flags_t *f)
 }
 
 /**
- * print_hex_big - prints a number in hexadecimal base,
- * in uppercase
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
+ * print_hex_big - prints a number in hexadecimal base
  * Return: the number of char printed
  */
 int print_hex_big(va_list l, flags_t *f)
